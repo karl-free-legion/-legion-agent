5631aef4fa3df35021f6247c2f33bb798c524e00
lance <lh160@zcsmart.com>
2019-03-22 19:25:44 +0800

    修改legion错误

--- src/main/java/com/zcs/legion/proxy/AgentHandler.java
+++ src/main/java/com/zcs/legion/proxy/AgentHandler.java
@@ -1,13 +1,14 @@
 package com.zcs.legion.proxy;
 
+import com.google.common.collect.Maps;
 import com.google.protobuf.ByteString;
-import com.legion.client.common.ActorProvider;
 import com.legion.client.common.IRecipientActor;
 import com.legion.client.common.RequestDescriptor;
-import com.legion.client.common.TagMatchers;
+import com.legion.client.common.annotation.ActorProvider;
 import com.legion.client.handlers.RecipientHandler;
-import com.legion.core.api.X;
+import com.legion.core.TagMatchers;
 import com.legion.core.exception.LegionException;
+import com.zcs.legion.api.A;
 import lombok.extern.slf4j.Slf4j;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.BoundRequestBuilder;
@@ -18,15 +19,21 @@ import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+/**
+ * 升级代理
+ * @author lance
+ * 2019.3.22 15:46
+ */
 @Slf4j
 @Component
-public class AgentHandler implements IRecipientActor<X.XHttpRequest> {
-    AsyncHttpClient httpClient = Dsl.asyncHttpClient();
+public class AgentHandler implements IRecipientActor<A.BrokerMessage> {
+    private AsyncHttpClient httpClient = Dsl.asyncHttpClient();
     @Autowired
     private AgentProperties agentProperties;
+
     @Override
-    @ActorProvider(matcher = TagMatchers.Any, name="agent_handler", model = X.XHttpRequest.class)
-    public void reactMessage(RequestDescriptor descriptor, X.XHttpRequest msg, RecipientHandler recipientHandler) {
+    @ActorProvider(matcher = TagMatchers.Any, name="agent_handler", model = A.BrokerMessage.class)
+    public void reactMessage(RequestDescriptor descriptor, A.BrokerMessage message, RecipientHandler recipientHandler) {
 
         Map<String, String> tagAgents = agentProperties.getTags();
         if (tagAgents == null) {
@@ -41,34 +48,40 @@ public class AgentHandler implements IRecipientActor<X.XHttpRequest> {
                     LegionException.valueOf(String.format("tag [%s] not found", descriptor.getTag())));
             return;
         }
-        String url = agentPrefix + msg.getRequestURI();
+        String url = agentPrefix + descriptor.getRequest().getRequestURI();
         log.debug("prepare post to url: {}", url);
         BoundRequestBuilder postBuilder = httpClient.preparePost(url);
         //http headers
-        Map<String, String> headers = msg.getHeadersMap();
+        Map<String, String> headers = descriptor.getRequest().getHeadersMap();
         for (String hk : headers.keySet()) {
             postBuilder.addHeader(hk, headers.get(hk));
         }
         //http body
-        postBuilder.setBody(msg.getBody());
-        //post
+        postBuilder.setBody(message.getBody());
         postBuilder.execute(new AgentHttpHandler() {
             @Override
             public String onCompleted(Response response) throws Exception {
                 log.debug("http response: {}", response);
-                X.XHttpResponse.Builder r = X.XHttpResponse.newBuilder();
+                Map<String, String> r = Maps.newHashMap();
                 for (Map.Entry<String, String> h : response.getHeaders()) {
-                    r.putHeaders(h.getKey(), h.getValue());
+                    r.put(h.getKey(), h.getValue());
                 }
-                r.setStatus(response.getStatusCode()).setBody(response.getResponseBody());
-                recipientHandler.setSuccess(r.build());
-                log.debug("http response to reply message: {}", r);
+
+                descriptor.setStatus(r);
+                A.BrokerMessage.Builder reply = A.BrokerMessage.newBuilder()
+                        .setCode(response.getStatusCode())
+                        .setBody(response.getResponseBody());
+                recipientHandler.setSuccess(reply.build(), descriptor);
+
+                if(log.isDebugEnabled()){
+                    log.debug("http response to reply message: {}", reply);
+                }
                 return response.getStatusText();
             }
             @Override
             public void onFailed(Throwable cause) {
                 recipientHandler.setFailure(LegionException.valueOf(cause));
-                log.debug("http response exception: ", cause);
+                log.warn("http response exception: ", cause);
             }
         });
     }
